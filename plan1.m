6a1916d93896f1eee36443b7b9326a7a17d97a61
weilinyin <weilinyin@mail.nwpu.edu.cn>
2024-10-07 00:39:45 +0800

    1

--- plan1.m
+++ plan1.m
@@ -1,41 +1,47 @@
-function plan1
+
     % 定义参数
-    k_H = 1; % 示例值
+    k_H = -1; % 示例值
     k_dot_H = 1; % 示例值
-    S_ref = 1; % 示例值
-    m = 1000; % 质量示例值
-    g = 9.81; % 重力加速度
-    P = 100; % 推力示例值
+    S_ref = 0.45; 
+    m = 300; 
+    g = 9.81; 
+    P = 2000; 
     
-    % 时间范围
-    tspan = [0 10]; % 0到10秒
-    init_conditions = [0; 0; 0; 0; 0; 0]; % [V; theta; x; y; delta_z; alpha]
+    %初始条件
+    init_conditions = [300; 0; 0; 7000; 0; 0 ;7000]; % [V; theta; x; y; delta_z; alpha; H*]
 
-    % ODE求解
-    [t, sol] = ode45(@(t, y) odefun(t, y, k_H, k_dot_H, S_ref, m, g, P), tspan, init_conditions);
+    %步长
+    dt=1;
+    %时长
+    ts=100;
 
+    % 求解
+    y = zeros(7, 100/ts);
+    y(:,1) = init_conditions;
+    dydt = zeros(7, 1);
+    for i=2:ts/dt
+
+        C_x = 0.2 + 0.005 * rad2deg(y(6))^2;
+        C_y = 0.25 * rad2deg(y(6)) + 0.05 * rad2deg(y(5));
+        q = dynamic_pressure(y(4), y(1));
+
+        X = C_x * q * S_ref; 
+        Y = C_y * q * S_ref; 
+
+        dydt(1) = (P * cos(y(6)) ) / m - X / m - g * sin(y(2)); %V
+        dydt(2) = (P * sin(y(6)) + Y - m * g * cos(y(2)-y(6))) / (m * y(1)); %theta 
+        dydt(3) = y(1) * cos(y(2)); % dx/dt
+        dydt(4) = y(1) * sin(y(2)); % dy/dt
+        y(5) = k_H * (y(7) - y(4)) + k_dot_H * (dydt(7) - dydt(4)); %delta_z
+        dydt(6) = 0.24 * y(5); %alpha
+        dydt(7) = 2000 * cos(0.000314 * 1.1 * y(3)) + 5000; %H*
+        y(:,i) = y(:,i-1) + dt*dydt; %积分
+    end
     % 结果可视化
     figure;
-    plot(t, sol(:, 1)); % 画速度
-    xlabel('Time (s)');
-    ylabel('Velocity (V)');
-    title('Velocity vs Time');
-end
+    plot(y(3, :), y(4, :)); % xy图
+    xlabel('X (m)');
+    ylabel('Y (m)');
+    title('X vs Y');
 
-function dydt = odefun(t, y, k_H, k_dot_H, S_ref, m, g, P)
-    H_star = 2000 * cos(0.000314 * 1.1 * y(5)) + 5000;
-    delta_z = k_H * (H_star - y(5)) + k_dot_H * (0 - y(5)); % 这里需要实际的 \dot{H} 值
-    C_x = 0.2 + 0.005 * y(6)^2;
-    C_y = 0.25 * y(6) + 0.05 * delta_z;
 
-    X = C_x * 1 * S_ref; % q 值在这里假设为1
-    Y = C_y * 1 * S_ref; % q 值在这里假设为1
-
-    dydt = zeros(6, 1);
-    dydt(1) = (P * cos(y(6)) * cos(0)) / m - X / m - g * sin(0); % theta 假设为0
-    dydt(2) = (P * sin(y(6)) + Y - m * g * cos(0)) / (m * y(1)); % theta 假设为0
-    dydt(3) = y(1) * cos(y(2)); % dx/dt
-    dydt(4) = y(1) * sin(y(2)); % dy/dt
-    dydt(5) = delta_z; % delta_z的变化率
-    dydt(6) = - (y(5) / 1); % alpha的变化率，假设 m_z^delta_z 和 m_z^alpha 为1
-end
