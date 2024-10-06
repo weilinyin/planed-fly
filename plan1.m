4bb3468635cff8558b3f14c275afb9bf47479bb9
weilinyin <weilinyin@mail.nwpu.edu.cn>
2024-10-07 01:00:13 +0800

    1

--- plan1.m
+++ plan1.m
@@ -12,31 +12,32 @@
                     % [V; theta; x; y; delta_z; alpha; H*]
 
     %步长
-    dt=1;
+    dt=0.1;
     %时长
-    ts=100;
+    ts=10;
 
     % 求解
     y = zeros(7, 100/ts);
     y(:,1) = init_conditions;
     dydt = zeros(7, 1);
-    for i=2:ts/dt
+    for i=1:ts/dt-1
 
-        C_x = 0.2 + 0.005 * rad2deg(y(6))^2;
-        C_y = 0.25 * rad2deg(y(6)) + 0.05 * rad2deg(y(5));
-        q = dynamic_pressure(y(4), y(1));
+        C_x = 0.2 + 0.005 * rad2deg(y(6,i))^2;
+        C_y = 0.25 * rad2deg(y(6,i)) + 0.05 * rad2deg(y(5,i));
+        q = dynamic_pressure(y(4,i), y(1,i));
 
         X = C_x * q * S_ref; 
         Y = C_y * q * S_ref; 
 
-        dydt(1) = (P * cos(y(6)) ) / m - X / m - g * sin(y(2)); %V
-        dydt(2) = (P * sin(y(6)) + Y - m * g * cos(y(2)-y(6))) / (m * y(1)); %theta 
-        dydt(3) = y(1) * cos(y(2)); % dx/dt
-        dydt(4) = y(1) * sin(y(2)); % dy/dt
-        y(5,i) = k_H * (y(7) - y(4)) ; %delta_z
-        dydt(6) = 0.24 * y(5); %alpha
-        y(7,i) = 2000 * cos(0.000314 * 1.1 * y(3)) + 5000; %H*
-        y(:,i) = y(:,i-1) + dt*dydt; %积分
+        dydt(1) = (P * cos(y(6,i)) ) / m - X / m - g * sin(y(2,i)); %V
+        dydt(2) = (P * sin(y(6,i)) + Y - m * g * cos(y(2,i)-y(6,i))) / (m * y(1,i)); %theta 
+        dydt(3) = y(1,i) * cos(y(2,i)); % dx/dt
+        dydt(4) = y(1,i) * sin(y(2,i)); % dy/dt
+        y(5,i+1) = k_H * (y(7,i) - y(4,i)) ; %delta_z
+        dydt(6) = 0.24 * y(5,i); %alpha
+        y(7,i+1) = 2000 * cos(0.000314 * 1.1 * y(3,i)) + 5000; %H*
+        y(1:4,i+1) = y(1:4,i) + dt*dydt(1:4); %积分
+        y(6,i+1) = y(6,i) + dt*dydt(6);
     end
     % 结果可视化
     figure;
