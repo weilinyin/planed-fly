a40008fd41b85831d1a6154d6add5a795e15f2ad
weilinyin <weilinyin@mail.nwpu.edu.cn>
2024-10-07 10:33:27 +0800

    1

--- plan2.m
+++ plan2.m
@@ -1,12 +1,12 @@
-function y=plan1(init_conditions)
+function y=plan2(init_conditions)
 % [V; theta; x; y; delta_z; alpha; H*; mass]
 %
     % 定义参数
     k_H = 1; % 示例值
     k_dot_H = 1; % 示例值
-    S_ref = 0.45;  
+    S_ref = 0.45; 
     g = 9.81; 
-    P = 0; 
+    P = 2000; 
     
 
     %步长
@@ -17,7 +17,7 @@ function y=plan1(init_conditions)
     y(:,1) = init_conditions;
     dydt = zeros(8, 1);
     i=1;
-    while(y(3,i)<9100 && y(1,i)>0)
+    while(y(3,i)<24000 && y(1,i)>0)
         C_x = 0.2 + 0.005 * rad2deg(y(6,i))^2;
         C_y = 0.25 * rad2deg(y(6,i)) + 0.05 * rad2deg(y(5,i));
         q = dynamic_pressure(y(4,i), y(1,i));
@@ -29,8 +29,8 @@ function y=plan1(init_conditions)
         dydt(2) = (P * sin(y(6,i)) + Y - y(8,i) * g * cos(y(2,i)-y(6,i))) / (y(8,i) * y(1,i)); %theta 
         dydt(3) = y(1,i) * cos(y(2,i)); % dx/dt
         dydt(4) = y(1,i) * sin(y(2,i)); % dy/dt
-        y(7,i+1) = 2000 * cos(0.000314 * 1.1 * y(3,i)) + 5000; %H*
-        y(5,i+1) = k_H * (y(7,i) - y(4,i)) + k_dot_H * (y(7,i+1)-y(7,i)-dydt(4)) ; %delta_z
+        y(7,i+1) = 3050; %H*
+        y(5,i+1) = k_H * (y(7,i) - y(4,i)) + k_dot_H * dydt(4) ; %delta_z
         if y(5,i+1)>deg2rad(15)
             y(5,i+1) = deg2rad(15);
         elseif y(5,i+1)<deg2rad(15)
@@ -38,7 +38,6 @@ function y=plan1(init_conditions)
         end
         y(6,i+1) = 0.24 * y(5,i); %alpha
         y(1:4,i+1) = y(1:4,i) + dt*dydt(1:4); %积分
-        y(8,i+1)= y(8,i) + dt*dydt(8);
         i=i+1;
     end
 end
