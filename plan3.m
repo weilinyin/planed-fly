30846c9466a229c8fe066844a89dc3bedae98f16
weilinyin <weilinyin@mail.nwpu.edu.cn>
2024-10-07 16:52:42 +0800

    plan3 final

--- plan3.m
+++ plan3.m
@@ -18,8 +18,6 @@ function y=plan3(init_conditions)
     dydt = zeros(8, 1);
     i=1;
 
-    [~,dydt(2)] = Proportional_Guidance(y(3,i), y(4,i), x_t, y_t, y(1,i), 0, y(2,i), 0, K);
-    y(2,1)= y(2,1) + dydt(2);
     while(y(4,i)>0 && sqrt((y(3,i)-x_t)^2+(y(4,i)-y_t)^2)>1 && y(1,i)>0)
         C_x = 0.2 + 0.005 * rad2deg(y(6,i))^2;
         C_y = 0.25 * rad2deg(y(6,i)) + 0.05 * rad2deg(y(5,i));
@@ -29,10 +27,10 @@ function y=plan3(init_conditions)
         Y = C_y * q * S_ref; 
 
         dydt(1) = (P * cos(y(6,i)) ) / y(8,i) - X / y(8,i) - g * sin(y(2,i)); %V
-        [q1,dydt(2)] = Proportional_Guidance(y(3,i), y(4,i), x_t, y_t, y(1,i), 0, y(2,i), 0, K);%theta 
+        [~,dydt(2)] = Proportional_Guidance(y(3,i), y(4,i), x_t, y_t, y(1,i), 0, y(2,i), 0, K);%theta 
         dydt(3) = y(1,i) * cos(y(2,i)); % dx/dt
         dydt(4) = y(1,i) * sin(y(2,i)); % dy/dt
-        y(6,i+1)=deg2rad((y(8,i) * y(1,i) * dydt(2)+y(8,i) * g * cos(y(2,i)))/(0.25 * q1 *S_ref))-0.05 * y(5,i);%alpha
+        y(6,i+1)=deg2rad((y(8,i) * y(1,i) * dydt(2)+y(8,i) * g * cos(y(2,i)))/(0.25 * q *S_ref))-0.05 * y(5,i);%alpha
         y(5,i+1)=0.1/0.024*y(6,i+1);%delta_z
         if y(5,i+1)>deg2rad(15)
             y(5,i+1) = deg2rad(15);
