7e92502c8c3685570d3d1b22ffe6c14e073d6303
weilinyin <weilinyin@mail.nwpu.edu.cn>
2024-10-07 10:10:54 +0800

    1

--- plan1.m
+++ plan1.m
@@ -1,6 +1,6 @@
 
     % 定义参数
-    k_H = 15; % 示例值
+    k_H = 1; % 示例值
     k_dot_H = 1; % 示例值
     S_ref = 0.45; 
     m = 300; 
@@ -12,12 +12,10 @@
                     % [V; theta; x; y; delta_z; alpha; H*]
 
     %步长
-    dt=0.1;
-    %时长
-    ts=10;
+    dt=0.01;
 
     % 求解
-    y = zeros(7, ts/dt);
+    y = zeros(7, 4680);
     y(:,1) = init_conditions;
     dydt = zeros(7, 1);
     i=1;
@@ -33,14 +31,14 @@
         dydt(2) = (P * sin(y(6,i)) + Y - m * g * cos(y(2,i)-y(6,i))) / (m * y(1,i)); %theta 
         dydt(3) = y(1,i) * cos(y(2,i)); % dx/dt
         dydt(4) = y(1,i) * sin(y(2,i)); % dy/dt
-        y(5,i+1) = k_H * (y(7,i) - y(4,i)) ; %delta_z
+        y(7,i+1) = 2000 * cos(0.000314 * 1.1 * y(3,i)) + 5000; %H*
+        y(5,i+1) = k_H * (y(7,i) - y(4,i)) + k_dot_H * (y(7,i+1)-y(7,i)-dydt(4)) ; %delta_z
         if y(5,i+1)>deg2rad(15)
             y(5,i+1) = deg2rad(15);
         elseif y(5,i+1)<deg2rad(15)
             y(5,i+1) = -deg2rad(15);
         end
         y(6,i+1) = 0.24 * y(5,i); %alpha
-        y(7,i+1) = 2000 * cos(0.000314 * 1.1 * y(3,i)) + 5000; %H*
         y(1:4,i+1) = y(1:4,i) + dt*dydt(1:4); %积分
         i=i+1;
     end
